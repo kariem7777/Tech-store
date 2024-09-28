b0fecfac1652d1500ae28836cceb7f6f0409723d
MINALOTFY10 <menalotfy665@gmail.com>
2024-09-28 20:04:09 +0300

    Product & Context Dependency Injection

---
+++ TechCommerce/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using TechCommerce.Models;
+
+namespace TechCommerce.Repositories
+{
+    public interface IProductRepository
+    {
+        public void Add(Product product);
+        public void Update(Product product);
+        public void Delete(int id);
+        public Product? GetbyId(int id);
+        public List<Product> GetAll();
+        public void Save();
+    }
+}
